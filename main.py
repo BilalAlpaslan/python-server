f89d1951b11f41fdfbc811eea9661141aaff888d
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-09-22 10:19:41 +0300

    hello world

--- main.py
+++ main.py
@@ -10,11 +10,21 @@ server.listen(5)
 while True:
     clientsocket, address = server.accept()
     print(f"Connection from {address} has been established!")
-    
+
     request = clientsocket.recv(1024).decode("utf-8")
     path = request.split(" ")[1]
-    
+
     print(f"Path: {path}")
-    
-    clientsocket.send(bytes("HTTP/1.1 200 OK", "utf-8"))
+
+    clientsocket.send(bytes(
+"""HTTP/1.1 200 OK \r
+Connection: keep-alive \r
+Date: Mon, 23 May 2016 12:54:48 GMT \r
+Server: Bilal/0.0.1 \r
+Content-Type: text/html; charset=utf-8 \r
+Content-Length: 12 \r
+Header: Example \r
+\r
+Hello World!
+""", "utf-8"))
     clientsocket.close()
