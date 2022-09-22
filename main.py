1d1296088701fa55492319e401603f3b557fd66e
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-09-22 10:08:38 +0300

    http 200 success

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ server.listen(5)
 while True:
     clientsocket, address = server.accept()
     print(f"Connection from {address} has been established!")
-    print(f'path: {clientsocket.recv(1024).decode("utf-8")}')
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    print("Sent Hello World to", address)
+    
+    request = clientsocket.recv(1024).decode("utf-8")
+    path = request.split(" ")[1]
+    
+    print(f"Path: {path}")
+    
+    clientsocket.send(bytes("HTTP/1.1 200 OK", "utf-8"))
     clientsocket.close()
