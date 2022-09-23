0f8a70eb2a31e4e5efb2aef5a16582b17d58d2e4
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-09-23 08:27:43 +0300

    lightning

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import socket
+from time import sleep
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server6 = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
 server.bind(('localhost', 8000))
+# server6.bind(('[0:0:0:0:0:0:0:1]', 8000))
 print(f"Server started on port 8000")
 
 server.listen(5)
+# server6.listen(5)
+
 
 while True:
     clientsocket, address = server.accept()
@@ -15,16 +20,64 @@ while True:
     path = request.split(" ")[1]
 
     print(f"Path: {path}")
+    
+    html = f"""<html>
+<head><title>Test</title></head><body><h1>Test  Path: {path}"""
 
-    clientsocket.send(bytes(
-"""HTTP/1.1 200 OK \r
+    raw = f"""HTTP/1.1 200 OK \r
 Connection: keep-alive \r
 Date: Mon, 23 May 2016 12:54:48 GMT \r
 Server: Bilal/0.0.1 \r
 Content-Type: text/html; charset=utf-8 \r
-Content-Length: 12 \r
+Content-Length: {len(html)+23+100} \r
 Header: Example \r
 \r
-Hello World!
+{html}
+"""
+
+    clientsocket.send(bytes(
+f"""HTTP\r
+\r
+{html}
 """, "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+    sleep(5)
+    clientsocket.send(bytes('-', "utf-8"))
+
+    sleep(5)
+    clientsocket.send(bytes('-</h1></body></html>', "utf-8"))
+
+    sleep(5)
     clientsocket.close()
+
