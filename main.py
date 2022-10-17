12fc30eb1da94bd23a41eb82206e73ac3fc8b30f
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-10-17 19:56:10 +0300

    example update

--- main.py
+++ main.py
@@ -2,24 +2,26 @@
 from pythonserver import Response, Router, Application
 
 app = Application("localhost", 8000)
-
 router = Router()
 
-
-@router.get("/")
+@app.get("/")
 def index(request):
     return Response(body="Hello World!", status_code=200, headers={"Header": "Value"})
 
 
-@router.get("/about")
+@app.get("/about")
 def about(request):
     return Response(body="About page", status_code=200, headers={"Header": "Value"})
 
 
-@router.get("/contact")
+@app.get("/contact")
 def contact(request):
     return Response(body="Contact page", status_code=200, headers={"Header": "Value"})
 
 
-app.router.add_router(router)
+@router.get("/test")
+def test(request):
+    return Response(body="Test page", status_code=200, headers={"Header": "Value"})
+
+app.add_router(router)
 app.run()
